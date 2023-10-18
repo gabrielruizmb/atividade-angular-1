fe67fb6d7b127cf76e0d9d73161854c1eb5bc15a
gabrielruizmb <gabrielruizmb@gmail.com>
2023-10-18 20:05:25 -0300

    Adicionado rotas e seus parâmetros para carros

--- front-end/src/app/cars/cars-details/cars-details.component.ts
+++ front-end/src/app/cars/cars-details/cars-details.component.ts
@@ -9,10 +9,12 @@ import { ActivatedRoute } from '@angular/router';
 export class CarsDetailsComponent {
 
   route = inject(ActivatedRoute);
+  formAction!: string;
 
   constructor() {
     let id = this.route.snapshot.paramMap.get('id');
     let action = this.route.snapshot.paramMap.get('action');
     console.log(id, action);
+    action === null ? this.formAction = 'cadastrar' : this.formAction = action;
   }
 }
